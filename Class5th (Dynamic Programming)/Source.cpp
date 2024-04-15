834e49f34225057089c87901c2e322bf270266c5
lim7635 <ju7635@naver.com>
2024-04-15 21:44:42 +0900

    2024.04.15 (동적 계획법)

---
+++ Class5th (Dynamic Programming)/Source.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#define VALUE 46
+using namespace std;
+
+int Fibonacci(int n, int list[]) // 또는 int * list
+{
+	if (n <= 0)
+	{
+		return 0;
+	}
+	else if (n <= 2)
+	{
+		list[n] = 1;
+		return 1;
+	}
+
+	if (list[n] != 0)
+	{
+		return list[n];
+	}
+
+	return list[n] = Fibonacci(n - 1, list) + Fibonacci(n - 2, list);
+}
+
+int main()
+{
+#pragma region 동적 계획법(Dynamic Programming)
+	// 특정 범위까지의 값을 구하기 위해서
+	// 그것과 다른 범위까지의 값을 이용하여 효율적으로 값을 구하는 알고리즘입니다.
+	// 동적 계획법의 시간 복잡도는 O(N)입니다.(메모이제이션을 하지 않을 경우 O(2^N))
+
+	// 메모이제이션(Memoization)
+	// 프로그램이 동일한 계산을 반복해야 할 때
+	// 이전에 계산한 값을 메모리에 저장함으로써
+	// 동일한 계산을 반복 수행하는 작업을 제거하여
+	// 프로그램의 실행 속도를 향상시키는 기법입니다.
+
+#pragma endregion
+
+	int list[VALUE + 1] = { 0, };
+
+	cout << Fibonacci(10, list) << endl;
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,82 +1,8 @@
 ﻿#include <iostream>
-#define SIZE 7
 using namespace std;
-
-void BinarySearch(int list[], int key)
-{
-	int left = 0;
-	int right = SIZE - 1;
-
-	while (left <= right)
-	{
-		// 1. 배열의 가운데 요소의 인덱스를 pivot으로 설정합니다.
-		int pivot = (left + right) / 2;
-
-		// 2. list[pivot]의 값이 찾고자하는 값과 같다면 검색을 종료합니다.
-		if (list[pivot] == key)
-		{
-			cout << "Key is Found : " << list[pivot] << endl;
-			return;
-		}
-
-		// 3. list[pivot]의 값이 찾고자하는 값보다 크다면 left ~ pivot 사이의 값을 검색합니다.
-		else if (list[pivot] > key)
-		{
-			right = pivot - 1;
-		}
-
-		// 4. list[pivot]의 값이 찾고자하는 값보다 크다면 right ~ pivot 사이의 값을 검색합니다.
-		else if (list[pivot] < key)
-		{
-			left = pivot + 1;
-		}
-	}
-
-	cout << "Key isn't Found" << endl;
-}
 
 int main()
 {
-#pragma region 이진 탐색(Binary Search)
-	// 순서가 정렬된 리스트에서 특정한 값의 위치를 찾는 알고리즘입니다.
-	// 이진 탐색의 시간 복잡도는 O(log N)입니다.
-
-	/*int list[SIZE] = { 5,6,11,13,27,55,88 };
-
-	BinarySearch(list, 11);*/
-	
-#pragma endregion
-
-#pragma region 소프트웨어 개발 원칙(SOLID)
-
-#pragma region 단일 책임 원칙(Single Responsibility Principle) SRP
-	// 하나의 객체는 반드시 하나의 책임만을 가지고 있도록 해야합니다.
-	// 하나의 클래스가 여러 책임을 가지고 있다면 클래스를 분리해야 합니다.
-
-#pragma endregion
-
-#pragma region 개방 폐쇄 원칙(Open Closed Principle) OCP
-	// 클래스는 확장에 대해서는 열러 있어야 하지만 수정에 대해서는 닫혀 있어야 합니다.
-
-#pragma endregion
-
-#pragma region 리스코프 치환 원칙(Liskov Substitution Principle) LSP
-	// 하위 클래스는 상위 클래스에서 가능한 행위를 수행할 수 있어야 하며
-	// 하위 클래스는 상위 클래스의 행위를 변경하지 않고 확장할 수 있어야 합니다.
-
-#pragma endregion
-
-#pragma region 인터페이스 분리 원칙(Interface Segregation Principle) ISP
-
-
-#pragma endregion
-
-#pragma region 의존성 역전 원칙(Dependency Inversion Principle) DIP
-
-
-#pragma endregion
-
-#pragma endregion
 
 	return 0;
 }
