181652d8084cae6a8b82b1a91a62a9f66faf2433
lim7635 <ju7635@naver.com>
2024-04-17 21:45:32 +0900

    2024.04.17 깊이 우선 탐색

---
+++ Class6th (Quick Sort)/Source.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#define SIZE 8
+using namespace std;
+
+void QuickSort(int list[], int start, int end)
+{
+	// 1. 종료 조건
+	if (start >= end)
+	{
+		return;
+	}
+
+	// 2. pivot 변수의 값을 설정합니다.(pivot은 배열의 가운데에 있는 값이 가장 선호됨)
+	int pivot = start;
+
+	// 3. left 변수의 값을 설정합니다.
+	int left = start + 1;
+
+	// 4. right 변수의 값을 설정합니다.
+	int right = end;
+
+	// 5. left가 right보다 크거나 같을 때까지 반복합니다.
+	while (left <= right)
+	{
+		// 6. left가 end보다 작거나 같고 list[left]가 list[pivot]보다 작거나 같을 때까지 반복합니다.
+		while (left <= end && list[left] <= list[pivot])
+		{
+			left++;
+		}
+
+		// 6. right가 start보다 크고 list[right]가 list[pivot]보다 크거나 같을 때까지 반복합니다.
+		while (right > start && list[right] >= list[pivot])
+		{
+			right--;
+		}
+
+		if (left > right)
+		{
+			swap(list[pivot], list[right]);
+		}
+		else
+		{
+			swap(list[left], list[right]);
+		}
+	}
+	QuickSort(list, start, right - 1);
+	QuickSort(list, right + 1, end);
+}
+
+int main()
+{
+#pragma region 퀵 정렬(분할 정복)
+	// 기준점을 획득한 다음 해당 기준점을 기준으로 배열을 나누고
+	// 한 쪽에는 기준점보다 작은 값들이 위치하고
+	// 다른 한 쪽에는 기준점보다 큰 값들이 위치하도록 합니다.
+	// 정렬되지 않은 배열 순서 정리할 때 용이
+
+	// 나누어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여
+	// 모든 배열이 기본 배열이 될 때까지 반복하면서 정렬하는 알고리즘입니다.
+	// 시간 복잡도 : 평균(n X log(n)), 최악(n^2)
+
+	int list[SIZE] = { 5,9,6,2,1,3,4,7 };
+
+	QuickSort(list, 0, SIZE - 1);
+	for (const int& element : list)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+	return 0;
+}
---
+++ Class7th (Depth First Search)/Source.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <vector>
+#define SIZE 9
+using namespace std;
+
+class Graph
+{
+private:
+	vector<int> graph[SIZE]; // 2차원 배열 벡터
+
+	bool visited[SIZE]; // 방문 확인 배열([0] 제외하고 넣음)
+
+public:
+	Graph()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			visited[i] = false;
+		}
+	}
+
+	void InsertEdge(int vertex, int edge)
+	{
+		graph[vertex].push_back(edge);
+	}
+
+	void DFS(int start) // 탐색 함수
+	{
+		// 현재 노드를 방문한 것으로 표시합니다.
+		visited[start] = true;
+
+		// 현재 노드를 출력합니다.
+		cout << start << " ";
+
+		// 현재 노드와 연결된 다른 노드를 재귀적으로 방문합니다.
+		for (int i = 0; i < graph[start].size(); i++)
+		{
+			if (visited[graph[start][i]] == false)
+			{
+				// 다음 노드가 방문하지 않은 노드라면 'DFS' 함수를 호출합니다.
+				DFS(graph[start][i]);
+			}
+		}
+	}
+};
+
+int main()
+{
+#pragma region 깊이 우선 탐색(Depth First Search) DFS
+	// root 노드부터 시작해서 다음 분기로 넘어가기 전에 해당 분기를 완벽하게 탐색하는 방법입니다.
+	// 길 찾기 알고리즘에 용이
+
+	// DFS는 스택 자료 구조를 사용합니다.
+	// 1. 시작 노드를 스택에 넣고 방문 처리합니다.
+
+	// 2. 스택의 최상단 노드에 방문하지 않은 인접 노드가 있다면 그 노드를 스택에 넣고 방문 처리합니다.
+
+	// 3. 방문하지 않은 인접 노드가 없으면 스택에서 최상단 노드를 꺼냅니다.
+
+	// 4. 더 이상 2번의 과정을 수행할 수 없을 때까지 반복합니다.
+
+	Graph graph;
+
+	graph.InsertEdge(1, 2);
+	graph.InsertEdge(1, 3);
+	graph.InsertEdge(1, 8);
+
+	graph.InsertEdge(2, 1);
+	graph.InsertEdge(2, 7);
+
+	graph.InsertEdge(3, 1);
+	graph.InsertEdge(3, 4);
+	graph.InsertEdge(3, 5);
+
+	graph.InsertEdge(4, 3);
+	graph.InsertEdge(4, 5);
+
+	graph.InsertEdge(5, 3);
+	graph.InsertEdge(5, 4);
+
+	graph.InsertEdge(6, 7);
+
+	graph.InsertEdge(7, 2);
+	graph.InsertEdge(7, 6);
+	graph.InsertEdge(7, 8);
+
+	graph.InsertEdge(8, 1);
+	graph.InsertEdge(8, 7);
+
+	graph.DFS(1); // EX) 1이 root 노드일 경우
+
+#pragma endregion
+
+	return 0;
+}
