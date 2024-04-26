e4a4088935af2a4960d42b39942722f74be5489e
lim7635 <ju7635@naver.com>
2024-04-26 21:55:17 +0900

    2024.04.26 다익스트라

---
+++ Class13th (Topological Sort)/Source.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#define SIZE 8
+using namespace std;
+
+class Graph
+{
+private:
+	vector<int> edge[SIZE];
+
+	int inDegree[SIZE];
+
+public:
+	Graph()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			inDegree[i] = 0;
+		}
+	}
+
+	void ConnectEdge(int x, int y)
+	{
+		edge[x].push_back(y);
+		inDegree[y]++;
+	}
+
+	void Sort()
+	{
+		queue<int> queue;
+		for (int i = 1; i < SIZE; i++)
+		{
+			if (inDegree[i] == 0)
+			{
+				queue.push(i);
+			}
+		}
+
+		for (int i = 1; i < SIZE; i++)
+		{
+			if (queue.empty() == true)
+			{
+				break;
+			}
+
+			int x = queue.front();
+
+			queue.pop();
+
+			cout << x << " ";
+
+			for (int j = 0; j < edge[x].size(); j++)
+			{
+				int y = edge[x][j];
+
+				inDegree[y]--;
+
+				if (inDegree[y] <= 0)
+				{
+					queue.push(y);
+				}
+			}
+		}
+	}
+
+};
+
+int main()
+{
+#pragma region 위상 정렬(Topological Sorting)
+	// 방향 그래프에 존재하는 각 정점들의 선행 순서를 지키며
+	// 모든 정점을 차례대로 진행하는 알고리즘입니다.
+
+	// 사이클이 발생하는 경우 위상 정렬을 수행할 수 없습니다.
+
+	// DAG(Directed Acyclic Graph) : 사이클이 존재하지 않는 그래프
+	// 시간 복잡도 : O(V + E)
+
+	// 위상 정렬
+	// 1. 진입 차수가 0인 정저을 Queue에 삽입한다.
+
+	// 2. Queue에서 원소를 꺼내 연결된 모든 간선을 제거합니다.
+
+	// 3. 간선 제거 이후에 진입 차수가 0이 된 정점을 Queue에 삽입합니다.
+
+	// 4. Queue가 비어있을 때까지 2번 ~ 3번을 반복 수행합니다.
+
+	Graph graph;
+
+	graph.ConnectEdge(1, 2);
+	graph.ConnectEdge(1, 5);
+
+	graph.ConnectEdge(2, 3);
+
+	graph.ConnectEdge(3, 4);
+	graph.ConnectEdge(3, 5);
+
+	graph.ConnectEdge(4, 6);
+
+	graph.ConnectEdge(5, 6);
+
+	graph.ConnectEdge(6, 7);
+
+	graph.Sort();
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,66 +1,95 @@
 ﻿#include <iostream>
-#include <vector>
-#include <queue>
-#define SIZE 8
+#include <limits.h>
+#define INFINITY 10000
+#define SIZE 5
 using namespace std;
 
 class Graph
 {
 private:
-	vector<int> edge[SIZE];
+	bool visited[SIZE];
 
-	int inDegree[SIZE];
+	int distance[SIZE];
+
+	int weight[SIZE][SIZE] =
+	{
+		{0,7,4,6,1},
+		{INFINITY,0,INFINITY,INFINITY,INFINITY},
+		{INFINITY,2,0,5,INFINITY},
+		{INFINITY,3,INFINITY,0,INFINITY},
+		{INFINITY,INFINITY,INFINITY,1,0}
+	};
+
+	int x;
+
+	int y;
 
 public:
 	Graph()
 	{
 		for (int i = 0; i < SIZE; i++)
 		{
-			inDegree[i] = 0;
+			visited[i] = false;
+			distance[i] = 0;
 		}
 	}
 
-	void ConnectEdge(int x, int y)
+	void CreateEdge(int x, int y, int count)
 	{
-		edge[x].push_back(y);
-		inDegree[y]++;
+		weight[x][y] = count;
 	}
 
-	void Sort()
+	// 최소 비용 노드를 탐색하는 함수
+	int SmallNode()
 	{
-		queue<int> queue;
-		for (int i = 1; i < SIZE; i++)
+		int min = INFINITY;
+		int minPosition = 0;
+		for (int i = 0; i < SIZE; i++)
 		{
-			if (inDegree[i] == 0)
+			if (min > distance[i] && visited[i] == false)
 			{
-				queue.push(i);
+				min = distance[i];
+				minPosition = i;
 			}
 		}
 
-		for(int i = 1; i < SIZE; i++)
+		return minPosition;
+	}
+
+	void Dijkstra(int start)
+	{
+		for (int i = 0; i < SIZE; i++)
 		{
-			if (queue.empty() == true)
+			distance[i] = weight[start][i];
+		}
+
+		visited[start] = true;
+
+		for (int i = 0; i < SIZE - 1; i++)
+		{
+			int minNode = SmallNode();
+
+			visited[minNode] = true;
+
+			for (int j = 0; j < SIZE; j++)
 			{
-				break;
-			}
-
-			int x = queue.front();
-
-			queue.pop();
-
-			cout << x << " ";
-
-			for (int j = 0; j < edge[x].size(); j++)
-			{
-				int y = edge[x][j];
-
-				inDegree[y]--;
-
-				if (inDegree[y] <= 0)
+				// 방문하지 않은 노드 중에서 시작점이 최소 비용 노드를 경유하는게 더 가까우면 값을 갱신합니다.
+				if (visited[j] == false)
 				{
-					queue.push(y);
+					if (distance[minNode] + weight[minNode][j] < distance[j])
+					{
+						distance[j] = distance[minNode] + weight[minNode][j];
+					}
 				}
 			}
+		}
+	}
+
+	void Show()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			cout << distance[i] << " ";
 		}
 	}
 
@@ -68,41 +97,29 @@ public:
 
 int main()
 {
-#pragma region 위상 정렬(Topological Sorting)
-	// 방향 그래프에 존재하는 각 정점들의 선행 순서를 지키며
-	// 모든 정점을 차례대로 진행하는 알고리즘입니다.
+#pragma region 다익스트라 알고리즘(Dijkstra)
+	// 시작점으로부터 모든 노드까지의 최소 거리를 구해주는 알고리즘입니다.
+	
+	// 1. distance 배열을 weight[시작점 노드]의 값들로 초기화 시켜줍니다.
 
-	// 사이클이 발생하는 경우 위상 정렬을 수행할 수 없습니다.
+	// 2. 시작점을 방문 처리합니다.
 
-	// DAG(Directed Acyclic Graph) : 사이클이 존재하지 않는 그래프
-	// 시간 복잡도 : O(V + E)
+	// 3. distance 배열에서 최소 비용 노드를 찾고 방문 처리합니다.
+	// 단, 이미 방문한 노드는 제외합니다.
 
-	// 위상 정렬
-	// 1. 진입 차수가 0인 정저을 Queue에 삽입한다.
+	// 4. 최소 비용 노드를 거쳐갈지 고려해서 distance 배열을 갱신합니다.
+	// 단, 이미 방문한 노드는 제외합니다.
 
-	// 2. Queue에서 원소를 꺼내 연결된 모든 간선을 제거합니다.
+	// 5. 모든 노드를 방문할 때까지 3번 ~ 4번을 반복합니다.
 
-	// 3. 간선 제거 이후에 진입 차수가 0이 된 정점을 Queue에 삽입합니다.
-
-	// 4. Queue가 비어있을 때까지 2번 ~ 3번을 반복 수행합니다.
+	// 방문하지 않은 노드 중에서 가장 작은 distance를 가진 노드를 방문하고
+	// 그 노드와 연결된 다른 노드까지의 거리를 계산합니다.
 
 	Graph graph;
 	
-	graph.ConnectEdge(1, 2);
-	graph.ConnectEdge(1, 5);
+	graph.Dijkstra(0);
 
-	graph.ConnectEdge(2, 3);
-
-	graph.ConnectEdge(3, 4);
-	graph.ConnectEdge(3, 5);
-
-	graph.ConnectEdge(4, 6);
-
-	graph.ConnectEdge(5, 6);
-
-	graph.ConnectEdge(6, 7);
-
-	graph.Sort();
+	graph.Show();
 
 #pragma endregion
 
