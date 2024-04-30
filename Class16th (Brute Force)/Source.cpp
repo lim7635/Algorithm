bf29b8d5ddc068d1f892150ac1be35a867b18cc3
lim7635 <ju7635@naver.com>
2024-04-30 21:45:39 +0900

    2024.04.30 백트래킹

---
+++ Class16th (Brute Force)/Source.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+#pragma region 브루트 포스(Brute Force)
+	// 가능한 모든 경우의 수를 모두 탐색하면서 결과를 도출하는 알고리즘입니다.
+
+	int password[3] = { 3,5,7 };
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			for (int k = 0; k < 10; k++)
+			{
+				if (i == password[0] && j == password[1] && k == password[2])
+				{
+					cout << "비밀번호 : " << i << " " << j << " " << k;
+					return 0;
+				}
+			}
+		}
+	}
+
+#pragma endregion
+
+	return 0;
+}
---
+++ Class17th (Back Tracking)/Source.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+void HanoiTower(int n, char from, char temp, char to)
+{
+	if (n == 1)
+	{
+		cout << "[" << n << "번 원판]" << from << " -> " << to << endl;
+	}
+	else
+	{
+		HanoiTower(n - 1, from, to, temp);
+		cout << "[" << n << "번 원판]" << from << " -> " << to << endl;
+		HanoiTower(n - 1, temp, from, to);
+	}
+}
+
+int main()
+{
+#pragma region 백트래킹(Back Tracking)
+	// 해를 찾아가는 도중에 지금의 경로가 해가 될 것 같지 않으면
+	// 더 이상 깊이 들어가지 않고 이전 단계로 다시 돌아가는 알고리즘입니다.
+
+	// 백트래킹의 유망성 판단
+	// 해가 될 만한지 판단한 후에 유망하지 않다고 결정되면
+	// 그 노드의 이전 노드로 돌아가 다음 자식 노드로 이동합니다.
+
+	// 해가 될 만한 가능성이 있으면 유망하다 (Promising)
+	// 유망하지 않은 노드에 가지 않는 것	  (Prunning)
+
+	HanoiTower(3, 'A', 'B', 'C');
+
+#pragma endregion
+
+	return 0;
+}
