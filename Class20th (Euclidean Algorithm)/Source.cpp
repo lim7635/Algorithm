bf7c83b53c72acf9d660f252ac56c08dc60d306b
lim7635 <ju7635@naver.com>
2024-08-02 21:45:59 +0900

    2024.08.02 유클리드 호제법

---
+++ Class20th (Euclidean Algorithm)/Source.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int Euclid(int x, int y)
+{
+	if (y == 0)
+	{
+		return x;
+	}
+	else
+	{
+		return Euclid(y, x % y);
+	}
+}
+
+int main()
+{
+#pragma region 유클리드 호제법
+	// 2개의 자연수 또는 정식의 최대 공약수를 구하는 알고리즘으로
+	// 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘입니다.
+	// 시간 복잡도 : O(Log N)
+
+	int A = 150;
+	int B = 108;
+	
+	cout << "두 수의 최대 공약수 : " << Euclid(max(A, B), min(A, B)) << endl;
+
+#pragma endregion
+
+	return 0;
+}
